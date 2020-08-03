c793ce47d860ee89ef14a421d7a92843ab4db751
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-08-03 15:50:19 +0100

    JavaScript FizzBuzz

---
+++ src/fizzBuzz.js
@@ -0,0 +1,30 @@
+class Fizzbuzz {
+  isDivisibleByThree(number) {
+    return this._isDivisibleBy(number, 3);
+  }
+
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+
+  isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
+  }
+
+  says(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return "FizzBuzz";
+    }
+    if (this.isDivisibleByThree(number)) {
+      return "Fizz";
+    }
+    if (this.isDivisibleByFive(number)) {
+      return "Buzz";
+    }
+    return number
+  }
+}
